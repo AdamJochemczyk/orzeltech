66310824b788047795f5df79d2f9ad44abd79a59
Adam Jochemczyk <adamjoche@gmail.com>
2020-12-23 21:45:45 +0100

    desktop design

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNavigation(){
+    var element=document.getElementById("navigation");
+    element.style.width="300px";
+}
+function closeNavigation(){
+    var element=document.getElementById("navigation");
+    element.style.width = "0px";
+}
